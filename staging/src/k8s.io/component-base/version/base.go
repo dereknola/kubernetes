1c7ff17c10043840abf3f243608eb3b3d153cad6
Derek Nola <derek.nola@suse.com>
2023-02-28 12:34:36 -0800

    v1.26.1-ds6

--- staging/src/k8s.io/component-base/version/base.go
+++ staging/src/k8s.io/component-base/version/base.go
@@ -3,8 +3,8 @@ package version
 var (
 	gitMajor = "1"
 	gitMinor = "26"
-	gitVersion   = "v1.26.1-ds5"
-	gitCommit    = "7f418bff0659e21d4d9abbcccbd08aec767fe399"
+	gitVersion   = "v1.26.1-ds6"
+	gitCommit    = "2446a07ff86f33658d606f87d0523089449133bb"
 	gitTreeState = "clean"
-	buildDate = "2023-02-28T20:23:16Z"
+	buildDate = "2023-02-28T20:33:15Z"
 )
